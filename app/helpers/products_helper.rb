fa95c8d65f92860bc172fa6e5994a4e91235f3c8
Kumar Abinash <abinash.b2@gmail.com>
2015-06-27 21:39:08 +0530

    Added kickstart css and js Added some styling from jumpstart lab validated numericality of price Alterred some of the form structures

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+	def print_price(price)
+		"Rs. #{price}"
+		# name_to_currency price
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+	validates_numericality_of :price
+
+	def price=(input)
+		input.delete!("$") #Note the !
+		super
+	end
 end
